6ac4aea75a1e735cf9cb93fced380aa58a85b4e7
AndyyHuang <handy1221@berkeley.edu>
2023-08-13 22:51:43 -0700

    hw5 uploaded

---
+++ CoinToss/CoinToss/CoinTossApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTossApp.swift
+//  CoinToss
+//
+//  Created by Andy Huang on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinTossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
